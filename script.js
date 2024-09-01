8d53dd5eb19fbd88ca1f0de718acbd4cd82374a1
Unuching marma <unuching.marma2023@gmail.com>
2024-09-01 08:08:56 +0600

    mouse up down attribute

---
+++ script.js
@@ -0,0 +1,8 @@
+function mDown(tx) {
+  tx.innerHTML = "Release Me";
+  tx.style.backgroundColor = "red";
+}
+function mUp(tx) {
+  tx.innerHTML = "Thank You";
+  tx.style.backgroundColor = "yellow";
+}
